98054ee737b6ec260fa3ba8bf944441b87e996a2
antipenko <sweetglory.victory@gmail.com>
2016-09-03 03:07:34 +0300

    'addFonts'

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     prefixer = require('gulp-autoprefixer'),
@@ -11,36 +11,36 @@ var gulp = require('gulp'),
     pngquant = require('imagemin-pngquant'),
     rigger = require('gulp-rigger'),
     browserSync = require("browser-sync"),
-    reload = browserSync.reload;;
+    reload = browserSync.reload;
 
 var path = {
     build: { //Тут мы укажем куда складывать готовые после сборки файлы
         html: 'build/',
         js: 'build/js/',
         css: 'build/css/',
-        img: 'build/img/',
+        img: 'build/image/',
         fonts: 'build/fonts/'
     },
     src: { //Пути откуда брать исходники
         html: 'app/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        js: 'app/js/app.js',//В стилях и скриптах нам понадобятся только main файлы
-        style: 'app/scss/**/*.scss',
-        img: 'app/image/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
-        fonts: 'app/fonts/**/*.*'
-    },
-    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-        html: 'app/**/*.html',
-        js: 'app/js/**/*.js',
-        style: 'app/scss/**/*.scss',
-        img: 'app/image/**/*.*',
-        fonts: 'app/fonts/**/*.*'
-    },
-    clean: 'build'
-};
-
+        js: 'app/js/',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'app/scss/**///*'.scss',
+       // img: 'app/image/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+      //  fonts: 'app/fonts/**/*.*'
+   //' },
+    //watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+   //     html: 'app/**/*.html',
+   //     js: 'app/js/**/*.js',
+   //     style: 'app/scss/**/*.scss',
+   //     img: 'app/image/**/*.*',
+    //    fonts: 'app/fonts/**/*.*'
+  //  },
+ //   clean: 'build'
+//};
+/*
 var config = {
     server: {
-        baseDir: "app"
+        baseDir: "build"
     },
     tunnel: true,
     host: 'localhost',
@@ -54,7 +54,7 @@ gulp.task('html:build', function () {
         .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
         .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 });
-gulp.task('js:build', function () {
+/*gulp.task('js:build', function () {
     gulp.src(path.src.js) //Найдем наш main файл
         .pipe(rigger()) //Прогоним через rigger
         .pipe(sourcemaps.init()) //Инициализируем sourcemap
@@ -63,6 +63,7 @@ gulp.task('js:build', function () {
         .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
         .pipe(reload({stream: true})); //И перезагрузим сервер
 });
+
 gulp.task('style:build', function () {
     gulp.src(path.src.style) //Выберем наш main.scss
         .pipe(sourcemaps.init()) //То же самое что и с js
@@ -84,9 +85,13 @@ gulp.task('image:build', function () {
         .pipe(gulp.dest(path.build.img)) //И бросим в build
         .pipe(reload({stream: true}));
 });
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
 gulp.task('build', [
     'html:build',
-    'js:build',
+    //'js:build',
     'style:build',
     'image:build'
 ]);
@@ -103,8 +108,38 @@ gulp.task('watch', function(){
     watch([path.watch.img], function(event, cb) {
         gulp.start('image:build');
     });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
 });
 gulp.task('webserver', function () {
     browserSync(config);
 });
 gulp.task('default', ['build', 'webserver', 'watch']);
+*/
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'); //Подключаем Sass пакет
+
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css/'))
+    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browserSync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'app/' // Директория для сервера
+        },
+        //notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+});
